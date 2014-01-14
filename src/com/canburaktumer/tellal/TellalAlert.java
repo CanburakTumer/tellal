b6d59c90d0d9ce36421b0b7c3c56f00c490adbf6
Canburakt <canbu@canbu.info>
2014-01-14 23:05:41 +0200

    TellalAlert fixed.

--- src/com/canburaktumer/tellal/TellalAlert.java
+++ src/com/canburaktumer/tellal/TellalAlert.java
@@ -12,9 +12,9 @@ public class TellalAlert {
 	public static void showAlert(Context context, JSONObject json){
 		try{
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle(json.getInt("title"))
-			.setMessage(json.getInt("message"))
-			.setPositiveButton(json.getInt("buttonText"), new DialogInterface.OnClickListener() {
+		builder.setTitle(json.getString("title"))
+			.setMessage(json.getString("message"))
+			.setPositiveButton(json.getString("buttonText"), new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog,int id) {
 					dialog.cancel();
 				}

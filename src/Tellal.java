d61bfb2606554e051ec44038d6016dbf6d1de409
Canburakt <canbu@canbu.info>
2014-01-14 21:11:17 +0200

    Basic classes written.

--- src/Tellal.java
+++ src/Tellal.java
@@ -1,4 +1,26 @@
+import android.os.AsyncTask;
 
-public class Tellal {
+
+public class Tellal extends AsyncTask<String, String, String>{
+
+	@Override
+	protected String doInBackground(String... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(String result) {
+		// TODO Auto-generated method stub
+		super.onPostExecute(result);
+	}
+
+	@Override
+	protected void onPreExecute() {
+		// TODO Auto-generated method stub
+		super.onPreExecute();
+	}
+	
+	
 
 }

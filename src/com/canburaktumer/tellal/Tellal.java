c3db53a81751917f03840f0810cd9f2c251d6168
Canburakt <canbu@canbu.info>
2014-01-14 22:02:53 +0200

    Functions completed.

--- src/com/canburaktumer/tellal/Tellal.java
+++ src/com/canburaktumer/tellal/Tellal.java
@@ -1,12 +1,23 @@
 package com.canburaktumer.tellal;
+import android.content.Context;
 import android.os.AsyncTask;
 
 
-public class Tellal extends AsyncTask<String, String, String>{
+public class Tellal extends AsyncTask<TellalConfig, String, String>{
+	
+	Context context;
+	
+	public Tellal(Context cntxt){
+		context = cntxt;
+	}
 
 	@Override
-	protected String doInBackground(String... params) {
+	protected String doInBackground(TellalConfig... params) {
 		// TODO Auto-generated method stub
+		TellalConfig config = params[0];
+		TellalReader reader = new TellalReader();
+		TellalAlert.showAlert(context, reader.getJSONFromUrl(config.getSourceURL()));
+		
 		return null;
 	}
 
--- src/com/canburaktumer/tellal/TellalAlert.java
+++ src/com/canburaktumer/tellal/TellalAlert.java
@@ -1,5 +1,27 @@
 package com.canburaktumer.tellal;
 
-public class TellalAlert {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class TellalAlert {
+	
+	public static void showAlert(Context context, JSONObject json){
+		try{
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setTitle(json.getInt("title"))
+			.setMessage(json.getInt("message"))
+			.setPositiveButton(json.getInt("buttonText"), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int id) {
+					dialog.cancel();
+				}
+			  });
+		}catch(JSONException je){
+			je.printStackTrace();
+		}
+	}
+	
 }
